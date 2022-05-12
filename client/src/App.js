0ffbd0ce581317c6ce29c708bac8437ae3a78a71
Vikas Kumar <vsrathod39@gmail.com>
2022-05-12 21:36:32 +0530

    login form front-end completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
 import "./App.css";
+import { ToastContainer } from "react-toastify";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Register from "./pages/register";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <ToastContainer />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/pages/register.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function register() {
+  return <div>Register</div>;
+}
+
+export default register;
